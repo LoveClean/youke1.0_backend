fb7ff7d0913b115a7bf07d93d9331edabf3b8447
linfengsheng <243110359@qq.com>
2018-03-01 11:53:10 +0800

    linfs

--- src/main/java/com/media/ops/backend/dao/mapper/DeviceMapper.java
+++ src/main/java/com/media/ops/backend/dao/mapper/DeviceMapper.java
@@ -2,6 +2,8 @@ package com.media.ops.backend.dao.mapper;
 
 import com.media.ops.backend.dao.entity.Device;
 
+import java.util.Map;
+
 public interface DeviceMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,10 @@ public interface DeviceMapper {
     int updateByPrimaryKeySelective(Device record);
 
     int updateByPrimaryKey(Device record);
+
+    ////////////////////以上是自动生成的////////////////////////////
+
+    //根据设备编号查询设备，add by linfs at 2018.3.1
+    Device selectByCode(String code);
+
 }
